b5605e6033b02bb9dac11756cc1032a5d0905d95
Pablo Tabares Hernandez <pablother3@gmail.com>
2018-08-16 09:40:01 -0500

    workshop components

--- main.js
+++ main.js
@@ -1,42 +1,59 @@
 const bridge = new Vue()
 
-
-Vue.component('pablo-component', {
-  props: ['people'],
-  template: '#parent',
+Vue.component('search-input', {
+  props: ['searchValue'],
+  template: `
+    <input type="search" :value="searchValue" @input="search($event.target.value)" placeholder="Search"/>
+  `,
   methods: {
-    chooseWinner() {
-      let peopleAmount = this.peopleList.length;
-      let index = Math.floor((Math.random() * peopleAmount));
-      bridge.$emit('winner-comp', this.people[index]);
-    }
-  },
-  data() {
-    return {
-      peopleList: this.people
+    search(searchValue) {
+      this.$emit('input', searchValue)
     }
   }
 })
 
-Vue.component('pablo-child-comp', {
-  template: `
-          <div> <h2>{{name}}</h2></div>
-        `,
-  data() {
-    return {
-      name: ''
-    }
-  },
-  created() {
-    bridge.$on('winner-comp', winnerGuy => {
-      this.name = winnerGuy;
+Vue.component('list-users', {
+  template: '#parent',
+  mounted() {
+    axios.get('https://randomuser.me/api/?results=50')
+    .then((result) => {
+      this.users = result.data.results.map(user => {
+        return {
+          name: user.name.first,
+          email: user.email,
+          pic: user.picture.medium
+        }
+      })
+    }).catch((err) => {
+      
     });
   },
+  data() {
+    return {
+      users: [],
+      searchValue: ''
+    }
+  },
+  computed: {
+    filteredValues() {
+
+      return this.users.filter( val => {
+        return val.name.includes(this.searchValue)
+      })
+    }
+  }
+})
+
+Vue.component('card-user', {
+  props: ['user'],
+  template: `
+          <li>
+            <img :src="user.pic" :alt="user.name"/>
+            {{user.name}}
+          </li>
+        `
 })
 
 const app = new Vue({
-  el: '#app1',
-  data: {
-    personas: ['Goku', 'Gohan', 'Picoro', 'Vegeta', 'Zeno Sama', 'Bills', 'Jiren']
-  }
+  el: '#app1'
 })
