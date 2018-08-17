b6ecdf980f5dfde900ad0e771a8e7833e12d5cc6
Pablo Tabares Hernandez <pablother3@gmail.com>
2018-08-17 18:46:13 -0500

    vuex state update

---
+++ vuex-inits/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <counter></counter>
+</template>
+
+<script>
+  import counter from './components/counter.vue';
+  export default {
+    components: { counter }
+  };
+  
+</script>
---
+++ vuex-inits/src/components/counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1 v-text="amountVar"></h1>
+    <button @click="increase">Increase</button>
+    <button @click="decrease">Decrease</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      increase() {
+        this.$store.state.amount++;
+      },
+
+      decrease() {
+        this.$store.state.amount--;
+      }
+    },
+    computed: {
+      amountVar() {
+        return this.$store.state.amount;
+      }
+    }
+  };
+  
+</script>
