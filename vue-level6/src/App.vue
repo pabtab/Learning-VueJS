9f1bfbffba6f9185534d31903a6d3b186ae1bd42
Pablo Tabares Hernandez <pablother3@gmail.com>
2018-08-20 11:17:43 -0500

    working with map state

---
+++ vue-level6/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{ getName }}</h1>
+</template>
+
+<script>
+import {mapState} from 'vuex';
+
+export default {
+  computed: mapState({
+    getName: state => state.name
+  })
+  /* computed: {
+    getName() {
+      return this.$store.state.name
+    }
+  } */
+};
+</script>
